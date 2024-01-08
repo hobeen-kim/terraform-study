a35ef5f42263ba2ac7c0a824711e98d93afa1ae7
hobenn-kim <sksjsksh32@naver.com>
2024-01-08 20:38:17 +0900

    3.7.1

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-variable "my_var" {
-  default = "Hello World"
+variable "prefix" {
+  default = "hello"
 }
 
-resource "local_file" "abc" {
-  content = var.my_var
-  filename = "${path.module}/abc.txt"
+locals {
+  name = "terraform"
+  content = "${var.prefix} ${local.name}"
+  my_info = {
+    age = 20
+    region = "KR"
+  }
+  my_nums = [1, 2, 3, 4, 5]
+}
+
+locals {
+  content = "content2" # local 변수는 중복 선언이 불가능하다.
 }
